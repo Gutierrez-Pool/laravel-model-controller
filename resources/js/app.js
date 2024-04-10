453c6bb3f46f2a881fcc1b2fa2112483e0d9234d
Gutierrez-Pool <152402955+Gutierrez-Pool@users.noreply.github.com>
2024-04-10 14:59:35 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+
+// Import our custom scss
+import '~resources/scss/app.scss'
+
+// precompilazione immagini
+import.meta.glob([
+    '../img/**'
+])
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts/app')
+
+@section('content')
+    Contenuto della pagina
+@endsection
